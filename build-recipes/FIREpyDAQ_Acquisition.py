920c2c59f8701d358a6ac805edd711ae9b9d7342
Dushyant Chaudhari <dushyant.chaudhari@ul.org>
2024-08-07 08:17:59 -0500

    poetry changes for pyinstaller (#27)

--- build-recipes/FIREpyDAQ_Acquisition.py
+++ build-recipes/FIREpyDAQ_Acquisition.py
@@ -1,8 +1,12 @@
-from firepydaq.acquisition.acquisitionNoDash import application
-from PySide6.QtWidgets import QApplication
-import sys
 
-app = QApplication(sys.argv)
-main_app = application()
-main_app.show()
-sys.exit(app.exec())
+if __name__ == '__main__':
+    import multiprocessing as mp
+    mp.freeze_support()
+    from firepydaq.acquisition.acquisitionNoDash import application
+    from PySide6.QtWidgets import QApplication
+    import sys
+
+    app = QApplication(sys.argv)
+    main_app = application()
+    main_app.show()
+    sys.exit(app.exec())
